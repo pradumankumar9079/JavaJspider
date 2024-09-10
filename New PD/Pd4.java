1001359e87df9739c3751c831daee3d85673bb24
pradumankumar9079 <pradumankumar9079@gmail.com>
2024-09-10 12:10:56 +0530

    day 1

--- New PD/Pd4.java
+++ New PD/Pd4.java
@@ -3,23 +3,23 @@
  */
 // public class Pd4 {
 
-//     public static void main(String[] args) {
+// public static void main(String[] args) {
 
-//         System.out.println("Program Started");
-//         int x = 10;
-//         // if (x + 20) {
-//         // } // it will generate error bcoz no boolean value
+// System.out.println("Program Started");
+// int x = 10;
+// // if (x + 20) {
+// // } // it will generate error bcoz no boolean value
 
-//         // if (5 > 10) {
-//         // System.out.println("5 Value is above 10"); // false will Be Skipped
-//         // }
+// // if (5 > 10) {
+// // System.out.println("5 Value is above 10"); // false will Be Skipped
+// // }
 
-//         if (10 > 5) {
-//             System.out.println("10 Value is above 5"); // true will Execute
-//         }
+// if (10 > 5) {
+// System.out.println("10 Value is above 5"); // true will Execute
+// }
 
-//         System.out.println("Program Ended");
-//     }
+// System.out.println("Program Ended");
+// }
 // }
 
 /**
@@ -28,58 +28,58 @@
  */
 // public class Pd4 {
 
-//     public static void main(String[] args) {
-//         System.out.println("Program Started");
-//         // int x = 10;
-//         // int y = 20;
-//         // if (x > y) {
-//         // System.out.println(x + " x is greater than y " + y);
-//         // }
-//         // if (x < y) {
-//         // System.out.println(y + " y is gerater than x " + x);
-//         // }
+// public static void main(String[] args) {
+// System.out.println("Program Started");
+// // int x = 10;
+// // int y = 20;
+// // if (x > y) {
+// // System.out.println(x + " x is greater than y " + y);
+// // }
+// // if (x < y) {
+// // System.out.println(y + " y is gerater than x " + x);
+// // }
 
-//         // using temp variable help in three number comparission
-//         int x = 10;
-//         int y = 20;
-//         int big = x;
-//         if (y > big)
-//             big = y;
-//         System.out.println(big);
-//         System.out.println("Program Ended ");
-//     }
+// // using temp variable help in three number comparission
+// int x = 10;
+// int y = 20;
+// int big = x;
+// if (y > big)
+// big = y;
+// System.out.println(big);
+// System.out.println("Program Ended ");
+// }
 // }
 
 /**
  * Program 3
  */
-public class Pd4 {
-    public static void main(String[] args) {
-        System.out.println("Program Started");
-        // int a = 2;
-        // int b = 3;
-        // int c = 4;
-        // if (a > b) {
-        // System.out.println(a + " is Greater");
-        // }
-        // if (b > c) {
-        // System.out.println(b + " is Greater");
-        // }
-        // if (c > a) {
-        // System.out.println(c + " is Greater");
-        // }
+// public class Pd4 {
+// public static void main(String[] args) {
+// System.out.println("Program Started");
+// // int a = 2;
+// // int b = 3;
+// // int c = 4;
+// // if (a > b) {
+// // System.out.println(a + " is Greater");
+// // }
+// // if (b > c) {
+// // System.out.println(b + " is Greater");
+// // }
+// // if (c > a) {
+// // System.out.println(c + " is Greater");
+// // }
 
-        // Other method
-        int a = 5;
-        int b = 20;
-        int c = 30;
-        int big = b;
-        if (a > big)
-            big = a;
-        if (c > big)
-            big = c;
-        System.out.println(big);
+// // Other method
+// int a = 5;
+// int b = 20;
+// int c = 30;
+// int big = b;
+// if (a > big)
+// big = a;
+// if (c > big)
+// big = c;
+// System.out.println(big);
 
-        System.out.println("Program Ended");
-    }
-}
+// System.out.println("Program Ended");
+// }
+// }
---
+++ New PD/Pd5.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+/**
+ * Program 1
+ */
+public class Pd5 {
+    static void checkEvenOdd(int num) {
+        if (num % 2 == 0)
+            System.out.println(num + " is Even");
+        else
+            System.out.println(num + " is Odd");
+
+    }
+
+    static void checkEvenOddUsingDivisibilty(int num) {
+        // int rem = num % 10;
+        // if (rem % 2 == 0) {
+        // System.out.println(num + " is Even");
+
+        // } else {
+        // System.out.println(num + " is Odd");
+
+        // }
+
+        // Tutor one
+        if ((num / 2) * 2 == num) {
+            System.out.println(num + " is Even");
+
+        } else {
+            System.out.println(num + " is Odd");
+
+        }
+    }
+
+    static void checkForLeapYear(int year) {
+        // Divisible by 4 or divisble by 100 and
+        if (year % 4 == 0) {
+            System.out.println(year + " is a Leap Year");
+        } else if (year % 400 == 0) {
+            if (year % 100 == 0) {
+                System.out.println(year + " is a Leap Year");
+            }
+        } else {
+            System.out.println(year + " is  not a Leap Year");
+        }
+    }
+
+    static void checkForN(int n) {
+        if (n > 10) {
+            System.out.println(n + " is greater than 10");
+        } else if (n < 10) {
+            System.out.println(n + " is Lesser than 10");
+        } else {
+            System.out.println(n + " is equal to 10");
+        }
+    }
+
+    public static void main(String[] args) {
+        // else should be always associated with if
+        // do not write anything between if/else
+        // Q1) Even odd Check
+        // User Input
+        // System.out.println("Enter number u wanna check for Even or Odd");
+        // Scanner s = new Scanner(System.in);
+        // int num = s.nextInt();
+        // checkEvenOdd(num);
+
+        // Q2) Even Odd Checking Without using Modulo
+        // System.out.println("Enter number u wanna check for Even or Odd");
+        // Scanner s = new Scanner(System.in);
+        // int num = s.nextInt();
+        // checkEvenOddUsingDivisibilty(num);
+
+        // Q3) check if year is leap year or not --> paartially Correct
+        // System.out.println("Enter Year u wanna check fo Leap");
+        // Scanner s = new Scanner(System.in);
+        // int year = s.nextInt();
+        // checkForLeapYear(year);
+
+        // Q4) check n is <, >, = to 10 // Three Posibility over here so else if Ladder
+        // used
+        System.out.println("Enter Number u wanna check for Greater Lesser or Equal to 10");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        checkForN(n);
+
+    }
+}
