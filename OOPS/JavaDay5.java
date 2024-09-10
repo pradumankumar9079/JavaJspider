9ed25f41faf87ed8363ff30525a179e3c1104bc8
pradumankumar9079 <pradumankumar9079@gmail.com>
2024-09-10 15:27:37 +0530

    day 1

---
+++ OOPS/JavaDay5.java
@@ -0,0 +1,13 @@
+/**
+ * Program 1 -- class name given by programmer and method as well it's is
+ * Identifier
+ */
+public class JavaDay5 {
+    public static void main(String[] args) {
+        int val = 10;
+
+        int $val = 10;
+        System.out.println($val);
+        System.out.println(val);
+    }
+}
