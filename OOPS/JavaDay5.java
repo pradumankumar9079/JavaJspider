97f33a047dfefa2c79326fa52d8b1ae50ea3661c
pradumankumar9079 <pradumankumar9079@gmail.com>
2024-09-10 15:52:47 +0530

    day 1

--- OOPS/JavaDay5.java
+++ OOPS/JavaDay5.java
@@ -1,13 +1,35 @@
 /**
- * Program 1 -- class name given by programmer and method as well it's is
- * Identifier
+ * Program 1 --> class name given by programmer and method as well it's is
+ * Identifier class Program1 --> correct class, 1Program --> Wrong, class
+ * P1rogram --> Correct,
+ * $ and _ Allowed But Not Recommended
+ * Key Word--> Not Allowed It's Reserved
+ * Blank Spaces are not Allowed like class Prime Number(){} --> NOT ALLOWED
+ */
+// public class JavaDay5 {
+// public static void main(String[] args) {
+
+// }
+// }
+
+/**
+ * Naming Convention --> PascalCase(First Letter of Each and Every Word is
+ * Capital like CredentialManager, class Login) for ClassName
+ */
+// camelCase(eg: test(),printBill, updateEmployeeSalary) first letter is small
+// thane wd3 Capital For Method And Variable(age,employeeId,creditCardNumber)
+// public class JavaDay5 {
+
+// public static void main(String[] args) {
+
+// }
+// }
+
+/**
+ * Scanner --> to take user input
  */
 public class JavaDay5 {
     public static void main(String[] args) {
-        int val = 10;
 
-        int $val = 10;
-        System.out.println($val);
-        System.out.println(val);
     }
 }
