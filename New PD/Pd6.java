765838724705b263f208e43d0c91567a329c8092
pradumankumar9079 <pradumankumar9079@gmail.com>
2024-09-11 11:06:47 +0530

    day 1

--- New PD/Pd6.java
+++ New PD/Pd6.java
@@ -1,9 +1,15 @@
 /**
- * Pd6
+ * // Logical Operator (&&--> (don't use single & it's Biitwise),||, !)--> it's
+ * Works on Boolean
+ * Expression and the
+ * result also in boolean
  */
 public class Pd6 {
 
     public static void main(String[] args) {
+        // Logical And(&&)--> Minimum Two Expression is mendatory--> if all conditon
+        // true return true else false
+        // Note:--> when all condition has to be met go for &&
 
     }
 }
