64009151f6debd19f920dacf3003d6579e0183d8
pradumankumar9079 <pradumankumar9079@gmail.com>
2024-09-17 18:29:06 +0530

    day 1

--- New PD/Pd10Practice.java
+++ New PD/Pd10Practice.java
@@ -1,26 +1,81 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
+// public class Pd10Practice {
+//     public static void main(String[] args) {
+//         // write a program to check user entered number is perfect number or not
+
+//         /*************************************************************************** */
+//         // after n/2 will get only one factor that's itself only so better to write n/2
+//         // instead of n
+//         /*************************************************************************** */
+
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter The number u wanna find for Perfect or not: ");
+//         int num = sc.nextInt();
+//         int sum = 0;
+
+//         for (int i = 1; i <= num / 2; i++) {
+//             if (num % i == 0) {
+//                 sum += i;
+//                 // System.out.print(i + " ");
+//             }
+//         }
+//         // System.out.println();
+//         if (sum == num) {
+//             System.out.println(num + " is a Perfect Number:");
+//         } else {
+//             System.out.println(num + " is not a perfect number:");
+//         }
+//     }
+// }
+// 6--> 1,2,3
+// 6 ,28,496 --> These are perfect number
+// 24 --> 1 2, 3,4 6,8,12
+
+/**
+ * Pd10Practice  Find Factorial of a given number try with recursion
+ */
+// import java.util.Scanner;
+
+// public class Pd10Practice {
+
+//     public static void main(String[] args) {
+//         System.out.println("Enter number u wanna find Factorial: ");
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         int fact = 1;
+
+//         // First Way
+//         // for (int i = 1; i <= n; i++) {
+//         // fact *= i;
+//         // }
+//         // Second Way Opposite looping it will also work don't declare fact variable
+//         // inside loop it will get initialized everytime --> Don't do it
+//         // it will not work for bigger number return garbage or 0 sometimes
+
+//         for (int i = n; i >= 2; i--)
+//             fact *= i;
+
+//         System.out.println(n + " Factorial is " + fact);
+//     }
+// }
+
+/**
+ * Pd10Practice --> Break means come out of particular loop
+ */
 public class Pd10Practice {
+
     public static void main(String[] args) {
-        // write a program to check user entered number is perfect number or not
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter The number u wanna find for Perfect or not: ");
-        int num = sc.nextInt();
-        int sum = 0;
-
-        for (int i = 1; i <= num / 2; i++) {
-            if (num % i == 0) {
-                sum += i;
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(i + " ");
+            for (int j = 1; j <= 5; j++) {
+                if (j == 2 || j == 4) {
+                    break;
+                }
+                System.out.print(j + " ");
+                System.out.println();
             }
-        }
-        if (sum == num) {
-            System.out.println(num + " is a Perfect Number:");
-        } else {
-            System.out.println(num + " is not a perfect number:");
         }
     }
 }
-
-// 6 ,28,496
-// 24 --> 1 2, 3,4 6,8,12
---
+++ OOPS/JavaDay11.java
@@ -0,0 +1,96 @@
+/**
+ * JavaDay11 String Day 1
+ */
+public class JavaDay11 {
+    public static void main(String[] args) {
+        String city = "Bangalore";
+        System.out.println(city);
+
+        /*************************************************************** */
+        // charAt()
+        String str = "Developer";
+        System.out.println(str.charAt(4)); // l
+        System.out.println(str.charAt(6)); // p
+        System.out.println(str.charAt(2)); // v
+        System.out.println(str.charAt(3)); // e
+        // System.out.println(str.charAt(12)); // Invalid Index
+        // (StringIndexOutOfBoundsException)
+        /*************************************************************** */
+
+        /*************************************************************** */
+        // Length --> In String lenght() is method
+        String s1 = "BTM";
+        System.out.println(s1.length()); // 3
+
+        String s2 = "Java_SE22";
+        System.out.println(s2.length()); // 9
+
+        // if i will insert one more space it will counted as 18 than
+        String s3 = "Software Engineer";
+        System.out.println(s3.length()); // 17
+        /*************************************************************** */
+
+        /*************************************************************** */
+        // toCharArray()
+        String str1 = "Tiger";
+        System.out.println("String is: " + str1);
+
+        // Converted into Character Array Listen it will won't affect original String
+        // str1
+        char[] ch = str1.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            System.out.print(ch[i] + " ");
+        }
+        System.out.println();
+        /*************************************************************** */
+
+        /*************************************************************** */
+        // indexOf() and lastIndexOf()
+        String str2 = "Developer";
+
+        // IndexOf--> Search L to R
+        System.out.println("Index Of");
+        System.out.print(str.indexOf('D') + " ");
+        System.out.print(str.indexOf('e') + " ");
+        System.out.print(str.indexOf('v') + " ");
+        System.out.print(str.indexOf('e') + " ");
+        System.out.print(str.indexOf('l') + " ");
+        System.out.print(str.indexOf('k') + " "); // Not Present -1 return
+
+        System.out.println();
+        // lastIndexOf()--> Search R to L
+        System.out.println("Last Index Of");
+        System.out.print(str.lastIndexOf('e') + " ");// 7 return last Occerence Index
+        System.out.print(str.lastIndexOf('D') + " ");
+        System.out.print(str.lastIndexOf('v') + " ");
+        System.out.print(str.lastIndexOf('l') + " ");
+        System.out.print(str.lastIndexOf('k') + " "); // Not Present -1 return
+        System.out.println();
+        /*************************************************************** */
+
+        /*************************************************************** */
+        // i wanna find all occuerence 1,2,3 -->Important
+        // String str2 = "Developer";
+        int a = str.indexOf('e'); // Seraching First Index
+        int b = str.indexOf('e', a + 1); // Seraching after First Occurenece (a+1)
+        int c = str.indexOf('e', b + 1); // Searching after Second Occurence With (b+1)
+
+        System.out.println("First Occurence: " + a);
+        System.out.println("Second Occurence: " + b);
+        System.out.println("Third Occurence: " + c);
+        /*************************************************************** */
+
+        /*************************************************************** */
+        // i wanna find all occuerence 1,2,3 -->Important
+        // String str2 = "Developer";
+        int d = str.lastIndexOf('e'); // Seraching First Index
+        int e = str.lastIndexOf('e', d - 1); // Seraching after First Occurenece (a-1)
+        int f = str.lastIndexOf('e', e - 1); // Searching after Second Occurence With (b-1)
+
+        System.out.println("First Occurence from Last: " + d);
+        System.out.println("Second Occurence from Last: " + e); 
+        System.out.println("Third Occurence from Last: " + f);
+        /*************************************************************** */
+
+    }
+}
